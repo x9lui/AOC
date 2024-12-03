033f07c56abdd2b94f701bc8c32e36c5fecb02ee
Luis Berciano Suárez <luis.bersua@alum.uca.es>
2024-12-03 15:26:05 +0100

    feat(24-day2-part2)

--- 2024/day2.c
+++ 2024/day2.c
@@ -41,15 +41,33 @@ int main(){
     char line[LINE_HEURISTIC_MAX_SIZE];
     char *number_str;
     int level[LEVEL_HEURISTIC_MAX_SIZE];
+    int sublevel[LEVEL_HEURISTIC_MAX_SIZE];
     int size;
     unsigned solution1 = 0;
+    unsigned solution2 = 0;
 
     while(fgets(line, LINE_HEURISTIC_MAX_SIZE, stdin) != NULL){
         size = str_to_int_array(line,level, " ");
+
+        //Part 1
         if(is_safe(level,size)){
             solution1++;
         }
+
+        //Part 2 - Try all level combinations removing 1 number
+        for(unsigned i = 0; i < size; i++){
+            for(unsigned j = 0; j < i; j++){
+                sublevel[j] = level[j];
+            }
+            for(unsigned j = i; j < size - 1; j++){
+                sublevel[j] = level[j+1];
+            }
+            if(is_safe(sublevel,size - 1)){
+                solution2++;
+                break;
+            }
+        }
     }
 
-    printf("part 1: %d\n", solution1);
+    printf("part 1: %d\npart 2: %d\n", solution1, solution2);
 }
