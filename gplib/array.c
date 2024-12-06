8253cbf69a82cec4cac41b46b788f54125f4478e
Luis Berciano Suárez <luis.bersua@alum.uca.es>
2024-12-06 19:48:04 +0100

    feat(gplib): str_to_unsigned_array()

--- gplib/array.c
+++ gplib/array.c
@@ -3,13 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-int str_to_int_array(char *s, int *arr, char *sep){
-    int size = 0;
+unsigned str_to_int_array(char *s, int *arr, char *sep){
+    unsigned size = 0;
     char *number_str;
 
-    number_str = strtok(s," ");
+    number_str = strtok(s,sep);
     while(number_str != NULL){
         arr[size] = atoi(number_str);
+        number_str = strtok(NULL, sep);
+        size++;
+    }
+
+    return size;
+}
+
+unsigned str_to_unsigned_array(char *s, unsigned *arr, char *sep){
+    unsigned size = 0;
+    char *number_str;
+
+    number_str = strtok(s,sep);
+    while(number_str != NULL){
+        arr[size] = (unsigned)atoi(number_str);
         number_str = strtok(NULL, sep);
         size++;
     }
--- gplib/array.h
+++ gplib/array.h
@@ -11,6 +11,18 @@
 *
 *   @return The number of elements in the array
 */
-int str_to_int_array(char *s, int *arr, char *sep);
+unsigned str_to_int_array(char *s, int *arr, char *sep);
+
+/*
+*   Converts an string represented array to an unsigned array,
+*   the numbers in the string are separated by a SEP
+*   character
+*   
+*   @param s the string represented array
+*   @param sep the separators between the numbers
+*
+*   @return The number of elements in the array
+*/
+unsigned str_to_unsigned_array(char *s, unsigned *arr, char *sep);
 
 #endif
